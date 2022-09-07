d0fdc4247fed4f12361af89f84ae1a483a1dbb40
Adam Żmuda <adamzureq@icloud.com>
2022-09-07 18:56:25 +0200

    Something

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Colors.transparent,
-                Colors.orangeAccent,
-              ],
-            ),
+            color: Color(0xFFFFFFFF),
           ),
           child: Column(
             children: [
@@ -36,44 +29,59 @@ class MyApp extends StatelessWidget {
                         style: TextStyle(
                           fontSize: 42,
                           fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                          color: Colors.black,
                         ),
                       ),
                     ),
                   ],
                 ),
               ),
+              Card(
+                color: Colors.yellow[700],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 300,
+                      height: 400,
+                      child: Padding(
+                        padding: const EdgeInsets.all(25.0),
+                        child: Image.asset('images/food.png'),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text("Meal of the day"),
+                    ),
+                  ],
+                ),
+              ),
+              const Spacer(
+                flex: 1,
+              ),
               Row(
                 children: [
-                  const Spacer(flex: 4),
-                  Card(
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          width: 250,
-                          height: 250,
-                          child: Padding(
-                            padding: const EdgeInsets.all(25.0),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(15.0),
-                              child: Image.asset('images/food.jpg'),
-                            ),
-                          ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text("Meal of the day"),
-                        ),
-                      ],
+                  const Spacer(
+                    flex: 1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: RawMaterialButton(
+                      onPressed: () {},
+                      fillColor: const Color(0xFF124e78),
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(15),
+                      child: const Icon(
+                        Icons.home,
+                        size: 25,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                  const Spacer(flex: 1),
                 ],
-              )
+              ),
             ],
           ),
         ),
